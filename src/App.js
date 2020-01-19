76cffd4747461a95b2381465e8a63302ac2ca4b9
AdeilsonESilva <adeilson@gmail.com>
2020-01-19 20:37:34 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useState } from "react";
+import { useLocalStore, useObserver } from "mobx-react";
+
+const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  const store = useLocalStore(() => ({
+    todos: ["Lavar carro"],
+    addTodo: todo => {
+      store.todos.push(todo);
+    },
+    get todoCount() {
+      return store.todos.length;
+    }
+  }));
+
+  return (
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+  );
+};
+
+const TodoHeader = () => {
+  const store = useContext(StoreContext);
+  return useObserver(() => <h1>{store.todoCount} de TODO</h1>);
+};
+
+const TodosList = () => {
+  const store = useContext(StoreContext);
+
+  return useObserver(() => (
+    <ul>
+      {store.todos.map(todo => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </ul>
+  ));
+};
+
+const TodosForm = () => {
+  const [todo, setTodo] = useState("");
+
+  const store = useContext(StoreContext);
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        store.addTodo(todo);
+        setTodo("");
+      }}
+    >
+      <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <TodoHeader />
+      <TodosList />
+      <TodosForm />
+    </StoreProvider>
   );
 }
 
